1ab15f172c0e883675f7be2d84da63957323b8a4
xgrinderx <ruslan.xolierov@mail.ru>
2026-09-23 22:09:02 +0300

    complete hw

---
+++ 2026_09_23_sort2vars_hw/simple_implementation/build_sort.bat
@@ -0,0 +1,15 @@
+@echo off
+chcp 1251 > log
+del log
+
+set MAIN=main.cpp
+set EXE=simple_sort_implementation.exe
+
+:: -fexec-charset=utf-8 в Windows не работает
+set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
+
+if exist %EXE% del %EXE%
+
+g++ "%CHARSET%" %MAIN% -o %EXE%
+
+%EXE%
---
+++ 2026_09_23_sort2vars_hw/simple_implementation/main.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <utility>
+
+void my_sort(int* arr, int size);
+void heap_s(int* arr, int n, int i);
+
+int main() {
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+    int* arr = new int[size];
+    std::cout << "Вводите элементы массива: ";
+    for (int i = 0; i < size; ++i) {
+        std::cin >> arr[i];
+    }
+    int* original = new int[size];
+    for (int i = 0; i < size; ++i) {
+        original[i] = arr[i];
+    }
+    my_sort(arr, size);
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << original[i] << (i + 1 < size ? ", " : "");
+    }
+    std::cout << std::endl;
+    std::cout << "Отсортированный: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << (i + 1 < size ? ", " : "");
+    }
+    std::cout << std::endl;
+    delete[] original;
+    delete[] arr;
+    return 0;
+}
+
+void heap_s(int* arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+        heap_s(arr, n, largest);
+    }
+}
+
+void my_sort(int* arr, int size) {
+    for (int i = size / 2 - 1; i >= 0; i--)
+        heap_s(arr, size, i);
+    for (int i = size - 1; i > 0; i--) {
+        std::swap(arr[0], arr[i]);
+        heap_s(arr, i, 0);
+    }
+}
